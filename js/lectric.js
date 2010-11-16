f384b8d7bbbaf2439040ff0e55d8ad6919d5b9de
Amos Lanka <amoslanka@gmail.com>
2010-11-16 13:49:32 -0800

    Some convenience methods. - Added a to(index) method - Added a method to go to an element. to_element(e) will scroll the slideshow to the specified element.

--- js/lectric.js
+++ js/lectric.js
@@ -144,6 +144,39 @@
     return flip * index * this.structure.itemWidth();
   };
 
+
+
+
+
+
+
+  BaseSlider.prototype.to = function(index) {
+    var previous = self.currentX;
+    this.currentX = this.limitXBounds(this.pageX(index));
+    if (this.currentX !== previous) {
+      this.update();
+    }
+    return false;
+  };
+
+  BaseSlider.prototype.to_element = function(e) {
+    var previous = self.currentX;
+    var index = this.get_item_index(e);
+	return this.to(index);
+  };
+	
+  BaseSlider.prototype.get_item_index = function(e) {
+	var all = this.element.find('.item');
+	for (var i=0; i < all.length; i++) {
+		if ($(all[i])[0] == e[0]) { return i; }
+	}
+  };
+
+
+
+
+
+
   BaseSlider.prototype.nextPageX = function(currentX) {
     if (this.page(currentX) + 1 <= this.structure.itemCount() - 1) {
       currentX = currentX -this.structure.itemWidth();
